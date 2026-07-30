9c4d9496e43d06e814acdb60aca457fd5bd888e5
fozen <67864043+fozenistaken@users.noreply.github.com>
2026-07-31 00:03:30 +0300

    Hotfix

--- MusicControl/index.tsx
+++ MusicControl/index.tsx
@@ -71,7 +71,7 @@ export default definePlugin({
     patches: [
         // ── Inject Player above the account panel ────────────────────────────
         {
-            find: ".DISPLAY_NAME_STYLES_COACHMARK),",
+            find: "#{intl::USER_PROFILE_ACCOUNT_POPOUT_BUTTON_A11Y_LABEL}",
             replacement: {
                 match: /(?<=\i\.jsxs?\)\()(\i),{(?=[^}]*?userTag:\i,occluded:)/,
                 replace: "$self.PanelWrapper,{VencordOriginal:$1,"
